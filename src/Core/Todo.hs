11a108ee23115e083b4c81523d6605dd7edd2fbe
Julien Lebrun <jl@exanet.nz>
2023-01-02 14:41:46 +0100

    refactoring

--- src/Core/Todo.hs
+++ src/Core/Todo.hs
@@ -3,43 +3,48 @@
 module Core.Todo (Todo (..), listTodo, createTodo) where
 
 import Control.Applicative (empty)
-import Data.Aeson (FromJSON(parseJSON), Value(Object), ToJSON(toJSON), (.:), object, KeyValue((.=)))
+import Data.Aeson
 import Data.Text (Text)
-import Database.SQLite.Simple (FromRow, fromRow, field, open, close, execute, query_)
+import Database.SQLite.Simple
 
-data Todo = Todo {
-    id :: Int,
-    description :: Text,
-    priority :: Int
-} deriving Show
+-- Entity
+data Todo = Todo
+  { getId :: Int,
+    getDescription :: Text,
+    getPriority :: Int
+  }
+  deriving (Show)
 
+-- JSON (de)serialization
 instance ToJSON Todo where
-    toJSON (Todo id description priority) = object [
-        "id" .= id,
-        "description" .= description,
-        "priority" .= priority
-        ]
+  toJSON (Todo uid description priority) =
+    object
+      [ "getId" .= uid,
+        "getDescription" .= description,
+        "getPriority" .= priority
+      ]
 
 instance FromJSON Todo where
-    parseJSON (Object v) = Todo <$> v .: "id"
-                                <*> v .: "description"
-                                <*> v .: "priority"
-    parseJSON _ = empty
+  parseJSON (Object v) =
+    Todo <$> v .: "getId"
+      <*> v .: "getDescription"
+      <*> v .: "getPriority"
+  parseJSON _ = empty
 
+-- SQLite mapping
 instance FromRow Todo where
-    fromRow = Todo <$> field
-                   <*> field
-                   <*> field
+  fromRow = Todo <$> field <*> field <*> field
 
+-- Database operations
 listTodo :: IO [Todo]
 listTodo = do
-    conn <- open "todo.db"
-    resp <- query_ conn "SELECT * FROM todo"
-    close conn
-    return resp
+  conn <- open "todo.db"
+  resp <- query_ conn "SELECT * FROM todo"
+  close conn
+  return resp
 
 createTodo :: Text -> Int -> IO ()
 createTodo description priority = do
-    conn <- open "todo.db"
-    execute conn "INSERT INTO todo (description, priority) VALUES (?,?)" (description, priority)
-    close conn
+  conn <- open "todo.db"
+  execute conn "INSERT INTO todo (description, priority) VALUES (?,?)" (description, priority)
+  close conn
--- src/Server.hs
+++ src/Server.hs
@@ -3,58 +3,61 @@
 module Server (serve) where
 
 import Control.Applicative (empty)
+import Core.Todo
 import Data.Aeson
-import Data.ByteString.Lazy (ByteString)
 import qualified Data.ByteString.Internal as B (ByteString)
+import qualified Data.ByteString.Lazy.Internal as LB (ByteString)
 import Data.Text (Text)
 import Network.HTTP.Types
 import Network.Wai
 import Network.Wai.Handler.Warp
 
-import Core.Todo (listTodo, createTodo)
-
+-- HTTP Header
 jsonContentType :: (HeaderName, B.ByteString)
 jsonContentType = (hContentType, "application/json")
 
-data CreateRequest = CreateRequest {
-    getDescription :: Text,
-    getPriority :: Int
-}
+-- Entity
+data CreateRequest = CreateRequest
+  { getDescriptionReq :: Text,
+    getPriorityReq :: Int
+  }
 
+-- JSON deserialization
 instance FromJSON CreateRequest where
-    parseJSON (Object v) = CreateRequest <$> v .: "description"
-                                         <*> v .: "priority"
-    parseJSON _ = empty
+  parseJSON (Object v) = CreateRequest <$> v .: "getDescriptionReq" <*> v .: "getPriorityReq"
+  parseJSON _ = empty
 
+-- Server
 serve :: IO ()
 serve = do
-    let port = 3000
-    putStrLn $ "Listening on port " ++ show port
-    run port app
+  let port = 3000
+  putStrLn $ "Listening on port " ++ show port
+  run port app
 
+-- Router
 app :: Application
-app req send = case rawPathInfo req of
-        "/" -> listRoute
-        "/create" -> createRoute req
-        _ -> notFoundRoute
-    >>= send
+app request send = route request >>= send
+  where
+    route req = case rawPathInfo req of
+      "/" -> listRoute
+      "/create" -> createRoute req
+      _ -> notFoundRoute
 
+-- Routes
 listRoute :: IO Response
-listRoute = responseLBS status200 [jsonContentType] . encode <$> listTodo
+listRoute = listTodo >>= respond status200 . encode
 
 createRoute :: Request -> IO Response
-createRoute req = doRespond <$> (doDecode req >>= doCreate)
+createRoute request = doDecode request >>= doCreate >>= doRespond
+  where
+    doDecode req = decode <$> lazyRequestBody req
+    doCreate (Just req) = createTodo (getDescriptionReq req) (getPriorityReq req) >> return (Right "201 - Created")
+    doCreate Nothing = return $ Left "400 - Bad request"
+    doRespond = either (respond status201) (respond status400)
 
 notFoundRoute :: IO Response
-notFoundRoute = return $ responseLBS status404 [jsonContentType] "404 - Not Found"
+notFoundRoute = respond status404 "404 - Not Found"
 
-doDecode :: Request -> IO (Maybe CreateRequest)
-doDecode req = decode <$> lazyRequestBody req
-
-doCreate :: Maybe CreateRequest -> IO (Either ByteString ByteString)
-doCreate (Just req) = createTodo (getDescription req) (getPriority req) >> return (Right "201 - Created")
-doCreate Nothing = return $ Left "400 - Bad request" 
-
-doRespond :: Either ByteString ByteString -> Response
-doRespond (Right msg) = responseLBS status201 [jsonContentType] msg
-doRespond (Left msg) = responseLBS status400 [jsonContentType] msg
+-- Helpers
+respond :: Status -> LB.ByteString -> IO Response
+respond status resp = return $ responseLBS status [jsonContentType] resp
